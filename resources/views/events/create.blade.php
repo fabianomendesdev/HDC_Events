1d5adf3a43795e267f623b2d67be245b0c39a040
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-04-26 23:06:40 -0300

    Class update 15

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -3,5 +3,32 @@
 @section('title', 'Criar Evento')
 
 @section('content')
-    <h1>Criar conteúdo da página</h1>
+
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie o seu evento</h1>
+    <form action="/events" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+        </div>
+        <div class="form-group">
+            <label for="city">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+        </div>
+        <div class="form-group">
+            <label for="private">O evento é privado?</label>
+            <select name="private" id="private" class="form-control">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="description">Descrição:</label>
+            <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Criar Evento">
+    </form>
+</div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
-
 Route::get('/events/create', [EventController::class, 'create']);
+Route::post('/events', [EventController::class, 'store']);
+
+
+
+
+
 
 Route::get('/login', [EventController::class, 'login']);
 
