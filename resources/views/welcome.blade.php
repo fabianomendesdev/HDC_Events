bc07417a715d4b1003c7f352ed71eb7e998777ae
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-05-18 23:16:51 -0300

    Class update 20

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
             <div class="card col-md-3">
                 <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
                 <div class="card-body">
-                    <p class="card-date">10/09/2020</p>
+                    <p class="card-date">{{ date("d/m/Y", strtotime($event->date)) }}</p>
                     <h5 class="card-title">{{ $event->title }}</h5>
                     <p class="card-participants">X-Participantes</p>
                     <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
@@ -27,6 +27,9 @@
             </div>
         @endforeach
     </div>
+    @if (count($events) == 0)
+        <p>Não há eventos disponíveis</p>
+    @endif
 </div>
 
 @endsection
