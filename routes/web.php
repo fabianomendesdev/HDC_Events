fdff38b775a686f3635a166150548163f1beea2e
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-05-20 19:53:00 -0300

    Class 25 update

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,4 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
