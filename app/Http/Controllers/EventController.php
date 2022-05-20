29e26a5c279536dfd7c8a4da06e8c2b2ef5aaa10
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-05-20 20:49:03 -0300

    Class 26 update

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -68,12 +68,19 @@ class EventController extends Controller
         return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
     }
 
-    public function dashboard() {
+    public function dashboard(){
 
         $user = auth()->user();
 
         $events = $user->events;
 
         return view('events.dashboard', ['events' => $events]);
+    }
+
+    public function destroy($id){
+
+        Event::findOrFail($id)->delete();
+
+        return redirect('/dashboard')->with('msg', 'Evento excluído com sucesso!');
     }
 }
