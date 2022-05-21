9d38bb74b488fe7a4241127226b576c8f27905a7
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-05-20 21:42:11 -0300

    Class 27 update

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -83,4 +83,34 @@ class EventController extends Controller
 
         return redirect('/dashboard')->with('msg', 'Evento excluído com sucesso!');
     }
+
+    public function edit($id){
+
+        $event = Event::findOrFail($id);
+
+        return view('events.edit', ['event' => $event]);
+    }
+
+    public function update(Request $request){
+
+        $data = $request->all();
+
+         // Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime('now')) . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $data['image'] = $imageName;
+        }
+
+
+        Event::findOrFail($request->id)->update($data);
+        
+        return redirect('/dashboard')->with('msg', "Evento editado com sucesso!");
+    }
 }
